293baa1e73d6185ed1da471243e60a7da6ddfbf9
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2022-06-09 13:50:41 +0700

    Fixing full Screen styles

--- src/pages/DailyAttendance.js
+++ src/pages/DailyAttendance.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <>
-      <div className="flex">
+      <div className="flex h-screen">
         <Sidebar />
         <div className="flex-1 flex flex-col">
           <Navbar />
